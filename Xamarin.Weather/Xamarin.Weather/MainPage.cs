9d6b19e61ce33e4655752018db946d90e62b800d
johnnymaikeo <johnnymaikeo@gmail.com>
2015-07-15 23:31:15 -0300

    Create Xamarin.Weather solution

---
+++ Xamarin.Weather/Xamarin.Weather/MainPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Xamarin.Weather
+{
+    class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Label label = new Label();
+            label.Text = "Greetings, Xamarin.Forms!";
+            this.Content  = label;
+        }
+    }
+}
