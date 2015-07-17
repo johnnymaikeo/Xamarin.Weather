2b1df9385cc47c4e442b55580cbea23b460d8de4
johnnymaikeo <johnnymaikeo@gmail.com>
2015-07-16 23:48:36 -0300

    End of day commit
    not that many progress on consuming data from a rest API service.

--- Xamarin.Weather/Xamarin.Weather/AddCityPage.cs
+++ Xamarin.Weather/Xamarin.Weather/AddCityPage.cs
@@ -1,22 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using Xamarin.Forms;
+using Xamarin.Weather.ViewModel;
 
 namespace Xamarin.Weather
 {
     class AddCityPage : ContentPage
     {
+        Entry CityName;
+
         public AddCityPage()
         {
-            Label pageTitle = new Label();
-            pageTitle.Text = "Add City";
+            Label pageTitle = new Label {
+                Text = "Add City",
+                FontSize = 42
+            };
+
+            this.CityName = new Entry
+            {
+                Placeholder = "City Name",
+            };
+
+            this.CityName.TextChanged += CityName_TextChanged;
 
             this.Content = new StackLayout
             {
+                Spacing = 0,
+                VerticalOptions = LayoutOptions.FillAndExpand,
                 Children =
                 {
                     pageTitle,
+                    this.CityName
                 }
             };
 
@@ -28,6 +44,14 @@ namespace Xamarin.Weather
             });
 
             this.ToolbarItems.Add(cancelToolbarItem);
+        }
+
+        public void CityName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (this.CityName.Text.Length >= 3) { 
+                AddCityViewModel vm = new AddCityViewModel();
+                vm.GetCityList(this.CityName.Text);
+            }
         }
     }
 }
---
+++ Xamarin.Weather/Xamarin.Weather/ViewModel/AddCityViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Xamarin.Weather.ViewModel
+{
+    class AddCityViewModel
+    {
+        public async void GetCityList(string name)
+        {
+            await FetchCitiesAsync(name);
+        }
+
+        public async Task FetchCitiesAsync(string name)
+        {
+            var url = string.Format("http://api.openweathermap.org/data/2.5/find?q={0}&type=like&sort=name", name);
+            
+            // Create an HTTP web request using the URL
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
+            request.ContentType = "application/json";
+            request.Method = "GET";
+
+            // Send the request to the server and wait for the response
+            using (WebResponse response = await request.GetResponseAsync())
+            {
+                // Get a stream representation of the HTTP web response
+                using (Stream stream = response.GetResponseStream())
+                {
+
+                }
+            }
+        }
+    }
+}
